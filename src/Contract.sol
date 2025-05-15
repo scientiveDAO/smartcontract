65d62cb66be132e6685d9c52ffe3b9f35c842e0d
Maycon-Rodrigues <maycon.zng@gmail.com>
2025-05-15 19:41:36 -0300

    first commit

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,6 +1,104 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Contract {
-    constructor() {}
+contract ScientiveDAO {
+    struct ProjectProposal {
+        string title;
+        string description;
+        string ipfsHash;
+        address payable scientist;
+        uint amountRequested;
+        uint votes;
+        bool funded;
+        mapping(address => bool) voters;
+    }
+
+    address public founder;
+    uint public quorum;
+    uint public proposalCount;
+    mapping(address => bool) public members;
+    mapping(uint => ProjectProposal) public proposals;
+
+    modifier onlyMembers() {
+        require(members[msg.sender], "Not a DAO member");
+        _;
+    }
+
+    constructor(uint _quorum) {
+        founder = msg.sender;
+        members[msg.sender] = true;
+        quorum = _quorum;
+    }
+
+    function joinDAO() external {
+        members[msg.sender] = true;
+    }
+
+    function contributeFunds() external payable onlyMembers {
+        // TODO: add a function to contribute funds to the DAO
+    }
+
+    function submitProject(
+        string memory _title,
+        string memory _description,
+        uint _amountRequested,
+        string memory _ipfsHash
+    ) external onlyMembers {
+        ProjectProposal storage p = proposals[proposalCount++];
+        p.title = _title;
+        p.description = _description;
+        p.scientist = payable(msg.sender);
+        p.amountRequested = _amountRequested;
+        p.ipfsHash = _ipfsHash;
+        p.funded = false;
+    }
+
+    function getProposals(
+        uint256 _startId,
+        uint256 _qtd
+    ) external view returns (ProjectProposal[] memory) {
+        ProjectProposal[] memory _projectProposal = new ProjectProposal[](_qtd);
+        uint256 _id = _startId;
+        uint256 count = 0;
+
+        do {
+            if (proposals[_id].funded == false) {
+                _projectProposal[count] = proposals[_id];
+                count++;
+            }
+
+            _id++;
+        } while (count < _qtd && _id <= proposalCount);
+
+        return _projectProposal;
+    }
+
+    function voteProject(uint proposalId) external onlyMembers {
+        ProjectProposal storage p = proposals[proposalId];
+        require(!p.voters[msg.sender], "Already voted");
+        require(!p.funded, "Already funded");
+        p.voters[msg.sender] = true;
+        p.votes++;
+    }
+
+    function fundProject(uint proposalId) external onlyMembers {
+        ProjectProposal storage p = proposals[proposalId];
+        require(!p.funded, "Already funded");
+        require(p.votes >= quorum, "Not enough votes");
+        require(
+            address(this).balance >= p.amountRequested,
+            "Insufficient DAO funds"
+        );
+
+        p.funded = true;
+        p.scientist.transfer(p.amountRequested);
+    }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+
+    function isMember(address addr) external view returns (bool) {
+        return members[addr];
+    }
 }
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,173 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/Contract.sol";
 
-contract ContractTest is Test {
-    function setUp() public {}
+contract ScientiveDAOTest is Test {
+    ScientiveDAO dao;
+    address founder;
+    address member1;
+    address member2;
+    address scientist;
+    uint quorum = 2;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        founder = address(this);
+        member1 = makeAddr("member1");
+        member2 = makeAddr("member2");
+        scientist = makeAddr("scientist");
+
+        vm.deal(founder, 10 ether);
+        vm.deal(member1, 10 ether);
+        vm.deal(member2, 10 ether);
+
+        dao = new ScientiveDAO(quorum);
+    }
+
+    function testConstructor() public {
+        assertEq(dao.founder(), founder);
+        assertEq(dao.quorum(), quorum);
+        assertTrue(dao.members(founder));
+    }
+
+    function testJoinDAO() public {
+        vm.prank(member1);
+        dao.joinDAO();
+
+        assertTrue(dao.members(member1));
+    }
+
+    function testContributeFunds() public {
+        vm.prank(member1);
+        dao.joinDAO();
+
+        vm.prank(member1);
+        dao.contributeFunds{value: 1 ether}();
+
+        assertEq(dao.getBalance(), 1 ether);
+    }
+
+    function testContributeFundsOnlyMembers() public {
+        vm.prank(member1);
+        vm.expectRevert("Not a DAO member");
+        dao.contributeFunds{value: 1 ether}();
+    }
+
+    function testSubmitProject() public {
+        vm.prank(scientist);
+        dao.joinDAO();
+
+        vm.prank(scientist);
+        dao.submitProject(
+            "Research Project",
+            "Conducting scientific research",
+            1 ether,
+            "ipfs://hash"
+        );
+
+        assertEq(dao.proposalCount(), 1);
+
+        (
+            string memory title,
+            string memory description,
+            string memory ipfsHash,
+            address payable scientistAddr,
+            uint amountRequested,
+            uint votes,
+            bool funded
+        ) = dao.proposals(0);
+
+        assertEq(title, "Research Project");
+        assertEq(description, "Conducting scientific research");
+        assertEq(ipfsHash, "ipfs://hash");
+        assertEq(scientistAddr, scientist);
+        assertEq(amountRequested, 1 ether);
+        assertEq(votes, 0);
+        assertEq(funded, false);
+    }
+
+    function testVoteProject() public {
+        // Setup project
+        vm.prank(scientist);
+        dao.joinDAO();
+
+        vm.prank(scientist);
+        dao.submitProject(
+            "Research Project",
+            "Conducting scientific research",
+            1 ether,
+            "ipfs://hash"
+        );
+
+        // Setup members
+        vm.prank(member1);
+        dao.joinDAO();
+
+        // Vote
+        vm.prank(member1);
+        dao.voteProject(0);
+
+        (, , , , , uint votes, ) = dao.proposals(0);
+        assertEq(votes, 1);
+    }
+
+    function testFundProject() public {
+        // Setup project
+        vm.prank(scientist);
+        dao.joinDAO();
+
+        vm.prank(scientist);
+        dao.submitProject(
+            "Research Project",
+            "Conducting scientific research",
+            1 ether,
+            "ipfs://hash"
+        );
+
+        // Setup members and vote
+        vm.prank(member1);
+        dao.joinDAO();
+
+        vm.prank(member2);
+        dao.joinDAO();
+
+        vm.prank(member1);
+        dao.voteProject(0);
+
+        vm.prank(member2);
+        dao.voteProject(0);
+
+        // Add funds to DAO
+        vm.prank(member1);
+        dao.contributeFunds{value: 2 ether}();
+
+        // Fund project
+        vm.prank(member1);
+        dao.fundProject(0);
+
+        (, , , , , , bool funded) = dao.proposals(0);
+        assertTrue(funded);
+        assertEq(dao.getBalance(), 1 ether);
+        assertEq(scientist.balance, 1 ether);
+    }
+
+    function testGetBalance() public {
+        vm.prank(member1);
+        dao.joinDAO();
+
+        vm.prank(member1);
+        dao.contributeFunds{value: 1 ether}();
+
+        assertEq(dao.getBalance(), 1 ether);
+    }
+
+    function testIsMember() public {
+        assertTrue(dao.isMember(founder));
+
+        vm.prank(member1);
+        dao.joinDAO();
+
+        assertTrue(dao.isMember(member1));
+        assertFalse(dao.isMember(member2));
     }
 }
